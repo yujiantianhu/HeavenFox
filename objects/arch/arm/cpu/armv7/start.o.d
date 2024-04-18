c5b734009da0ed6f93db0a4c55ddeff4d8c5ceba
talented-rain <yujiantianhu@163.com>
2024-04-19 00:19:31 +0800

    simply submit

--- objects/arch/arm/cpu/armv7/start.o.d
+++
@@ -1,7 +0,0 @@
-start.o: cpu/armv7/start.S \
- /usr/local/arm/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/libc/usr/include/stdc-predef.h \
- /home/ubuntu/ProgramWorks/MyDesign/Gardens/include/common/linkage.h \
- /home/ubuntu/ProgramWorks/MyDesign/Gardens/configs/mach_configs.h \
- /home/ubuntu/ProgramWorks/MyDesign/Gardens/configs/mach/imx6ull_toppet_config.h \
- /home/ubuntu/ProgramWorks/MyDesign/Gardens/arch/arm/include/asm/armv7/asm_config.h \
- /home/ubuntu/ProgramWorks/MyDesign/Gardens/include/board/board_config.h
--- objects/rootfs/fatfs/ff.o.d
+++
@@ -1,4 +0,0 @@
-ff.o: ff.c \
- /usr/local/arm/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/libc/usr/include/stdc-predef.h \
- ff.h integer.h ffconf.h diskio.h \
- /usr/local/arm/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/stdarg.h
